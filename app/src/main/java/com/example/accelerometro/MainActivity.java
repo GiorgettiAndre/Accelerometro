403bcae2fc1ba130f907ab0e4d745e650ab3ce30
Andrea Giorgetti <andreagiorgetti0@gmail.com>
2025-04-03 08:59:27 +0200

    Sensore aggiunto

--- app/src/main/java/com/example/accelerometro/MainActivity.java
+++ app/src/main/java/com/example/accelerometro/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.accelerometro;
 
-import android.os.Bundle;
+import static androidx.core.content.ContextCompat.getSystemService;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Path;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Sensore sensore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sensore = new Sensore("sensore1", (SensorManager) getSystemService(Context.SENSOR_SERVICE));
     }
 }
